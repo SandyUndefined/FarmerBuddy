719837c01b8247ce11433a9f05dbd151297e6d86
Sandy_undefined <08sandysk@gmail.com>
2024-12-20 17:03:20 +0530

    remove unused lines

--- lib/core/services/weather_service.dart
+++ lib/core/services/weather_service.dart
@@ -6,7 +6,6 @@ class WeatherService {
   final String apiKey = dotenv.env['weatherApiKey'] ?? '';
 
   Future<Map<String, dynamic>> fetchWeather(double lat, double lon) async {
-    print("weatherAPIKey - $apiKey");
     final url =
         "https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=$apiKey&units=metric";
     final response = await http.get(Uri.parse(url));
